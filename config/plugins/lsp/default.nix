794b93a5f22e305e487cb29cd90458e7b1d322c4
TheRiceCold <dalewaltergh@gmail.com>
2024-10-01 14:03:54 +0800

    refactor: lsp

--- config/plugins/lsp/default.nix
+++ config/plugins/lsp/default.nix
@@ -1,24 +1,28 @@
 {
   plugins = {
-    lspsaga = import ./lspsaga.nix;
-    # none-ls = import ./none-ls.nix;
-    lsp-format = {
-      enable = true;
-      lspServersToEnable = [ "nixd" "efm" ];
-    };
-
     lsp = {
       enable = true;
       servers = import ./servers.nix;
       keymaps = {
         lspBuf = {
           K = "hover";
-          gd = "definition";
-          gD = "references";
-          gi = "implementation";
+          # Replaced by glance.nvim
+          # gd = "definition";
+          # gD = "references";
+          # gi = "implementation";
         };
       };
       enabledServers = [];
     };
+
+    # Shows function signature when u type
+    # lsp-signature.enable = true;
+
+    # Diagnostics
+    trouble.enable = true;
+
+    # lint = import ./lint.nix;
   };
+  # Rename
+  # Action
 }
