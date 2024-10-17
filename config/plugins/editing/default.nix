515b61777b790398d0bc9049fbe8749169d7c779
TheRiceCold <dalewaltergh@gmail.com>
2024-10-18 06:33:41 +0800

    chore: autopairs and vim_current_word

--- config/plugins/editing/default.nix
+++ config/plugins/editing/default.nix
@@ -1,6 +1,17 @@
 { pkgs, ... }: {
   plugins = {
-    # nvim-autopairs = import ./autopairs.nix;
+    nvim-autopairs = {
+      enable = true;
+      settings = {
+        mapBs = false;
+        checkTs = true;
+        tsConfig = {
+          lua = ["string" "source"];
+          javascript = ["string" "template_string"];
+        };
+        disabledFiletypes = ["TelescopePrompt" "minifiles"];
+      };
+    };
 
     mini.modules = {
       move = {};
@@ -14,11 +25,16 @@
 
   extraPlugins = let
     builds = import ../builds.nix pkgs;
-  in [builds.scrollEOF];
+  in [builds.scrollEOF pkgs.vimPlugins.vim_current_word];
 
   extraConfigLua = ''
     require'scrollEOF'.setup{
       disabled_filetypes = { 'minifiles' }
     }
+
+    vim.cmd [[
+      let g:vim_current_word#highlight_twins = 0
+      let g:vim_current_word#excluded_filetypes = ['minifiles']
+    ]]
   '';
 }
